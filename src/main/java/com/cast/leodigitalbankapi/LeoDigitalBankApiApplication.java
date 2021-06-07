f7342e6d177e55e3026204c38cbd714a31920008
leonildomello42 <leonildo.mello@gmail.com>
2021-06-07 13:11:27 -0300

    Criando projeto

---
+++ src/main/java/com/cast/leodigitalbankapi/LeoDigitalBankApiApplication.java
@@ -0,0 +1,13 @@
+package com.cast.leodigitalbankapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeoDigitalBankApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeoDigitalBankApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cast/leodigitalbankapi/LeoDigitalBankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cast.leodigitalbankapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeoDigitalBankApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
